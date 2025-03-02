810b8a40837cbcf75bbeed539324a37e7ccc5f5a
Filip Hruška <XHRUF007@studenti.czu.cz>
2025-03-02 12:53:49 +0100

    first commit of work during a school lesson

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace UI_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(743, 13);
+            label1.Margin = new Padding(5, 0, 5, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(161, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Score: 0";
+            label1.TextAlign = ContentAlignment.TopRight;
+            label1.Click += label1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Red;
+            pictureBox2.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox2.Cursor = Cursors.AppStarting;
+            pictureBox2.Location = new Point(300, 300);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(150, 100);
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            pictureBox2.Tag = "MyWarrior";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(18F, 37F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(600, 600);
+            Controls.Add(pictureBox2);
+            Controls.Add(label1);
+            Font = new Font("Consolas", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 4, 5, 4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI_test
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft, goRight, jumping, isGameOver;
+        //int jumpSpeed;
+        //int force;
+        string score = "";
+        int playerSpeed = 5;
+        //int horizontalSpeed = 5;
+        //int verticalSpeed = 3;
+        //int enemyOneSpeed = 5;
+        //int enemyTwoSpeed = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
